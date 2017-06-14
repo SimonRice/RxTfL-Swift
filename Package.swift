a36e0baaf3085360fe7c0cc4e11befda440eaddf
Simon Rice <im@simonrice.com>
2017-06-14 01:28:39 +0100

    SPM correction

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "RxTfL",
     dependencies: [
-        .Package(url: "https://github.com/Thomvis/alamofire/alamofire.git", majorVersion: 4),
+        .Package(url: "https://github.com/alamofire/alamofire.git", majorVersion: 4),
         .Package(url: "https://github.com/ReactiveX/RxSwift.git", majorVersion: 3),
     ]
 )
